dac98239c4b67073d9e17f6bc87e3d52ec20f3e3
Kal Academy <info@kalacademy.org>
2018-10-27 12:55:38 -0700

    Redux part 2

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import axios from 'axios';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { connect } from 'react-redux';
+import { registeruser } from '../../actions/authActions';
 
 class Register extends Component {
   constructor() {
@@ -30,17 +32,19 @@ class Register extends Component {
       password2: this.state.password2
     };
 
-    axios.post('/api/users/register', newUser)
-    .then(res => console.log(res.data))
-    .catch(err => this.setState({errors: err.response.data}));
+    this.props.registeruser(newUser);
+
 
   }
 
   render() {
-    const {errors} = this.state;
+    const {errors} = this.props;
+
+    const { user } = this.props.auth;
 
     return (
       <div className="register">
+      {user ? user.name : null}
       <div className="container">
         <div className="row">
           <div className="col-md-8 m-auto">
@@ -84,4 +88,15 @@ class Register extends Component {
   }
 }
 
-export default Register;
+Register.propTypes = {
+  registeruser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+})
+
+export default connect(mapStateToProps, {registeruser})(Register);
